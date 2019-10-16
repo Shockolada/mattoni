edb45d72f37840eee20e0dc27e569a9aacf72896
Shockolada <shockolada@gmail.com>
2019-10-16 17:47:56 +0300

    16-10

--- source/js/main.js
+++ source/js/main.js
@@ -1,52 +1,80 @@
 $(document).ready(function () {
-//   $('.open-modal-quote').click(function () {
-//     event.preventDefault();
-//     $('.modal-quote').addClass('active');
-//     $('.overlay').fadeIn(200);
-//   });
 
-//   $('.overlay').click(function () {
-//     event.preventDefault();
-//     $(this).fadeOut(200);
-//     $('.modal').removeClass('active');
-//   });
+  if ($(window).width() < 940) {
+    $('.product__hover-area').click(function () {
+      console.log('+');
+      $(this).parent().find('.product__tooltip').fadeIn(200);
+      // $('.overlay').fadeIn(200);
+    });
+  } else {
+    // $('.product__hover-area').mouseenter(function () {
+    //   console.log('++');
+    //   $(this).parent().find('.product__tooltip').fadeIn(100);
+    // });
+    // $('.product__hover-area').mouseleave(function () {
+    //   $(this).parent().find('.product__tooltip').fadeOut(100);
+    // });
+  }
 
-//   $('.modal__close-btn').click(function () {
-//     event.preventDefault();
-//     $('.overlay').fadeOut(200);
-//     $('.modal').removeClass('active');
-//   });
+  $(window).resize(function () {
+    if ($(window).width() < 940) {
+      $('.product__hover-area').click(function () {
+        console.log('+');
+        $(this).parent().find('.product__tooltip').fadeIn(200);
+        // $('.overlay').fadeIn(200);
+      });
+    } else {
+
+    }
+  });
+
+  $('.close-tooltip').click(function () {
+    event.preventDefault();
+    $('.overlay').fadeOut(200);
+    $(this).closest('.product__tooltip').fadeOut(200);
+  });
+
+
+
+  //   $('.overlay').click(function () {
+  //     event.preventDefault();
+  //     $(this).fadeOut(200);
+  //     $('.modal').removeClass('active');
+  //   });
+
+  // $('.modal__close-btn').click(function () {
+  //   event.preventDefault();
+  //   $('.overlay').fadeOut(200);
+  //   $('.modal').removeClass('active');
+  // });
 
   $('.menu__btn').click(function () {
     event.preventDefault();
-    // $('.menu').toggleClass('active');
     $('.menu').slideToggle(200);
     $(this).toggleClass('active');
   });
 
-//   $('.service-info__name').click(function () {
-//     $('.service-info__name').removeClass('active');
-//     $(this).addClass('active');
 
-//     if ($(window).width() < 940) {
-//       $('.service-info__text').stop().slideUp(200).removeClass('active');
-//       $(this).next('.service-info__text').toggleClass('active').stop().slideToggle(200);
-//     }
-//     $('.service-info__text').removeClass('active');
-//     $(this).next('.service-info__text').addClass('active');
-//   });
 
-//   $(window).resize(function () {
-//     if ($(window).width() >= 940) {
-//       $('.service-info__text').css('display', 'block');
-//     } else {
-//       $('.service-info__text').css('display', 'none');
-//     }
-//   });
+  //   $('.service-info__name').click(function () {
+  //     $('.service-info__name').removeClass('active');
+  //     $(this).addClass('active');
 
-//   new WOW().init();
+  //     if ($(window).width() < 940) {
+  //       $('.service-info__text').stop().slideUp(200).removeClass('active');
+  //       $(this).next('.service-info__text').toggleClass('active').stop().slideToggle(200);
+  //     }
+  //     $('.service-info__text').removeClass('active');
+  //     $(this).next('.service-info__text').addClass('active');
+  //   });
 
-//   $(".service-preview__title--horizontal").lettering();
+  //   $(window).resize(function () {
+  //     if ($(window).width() >= 940) {
+  //       $('.service-info__text').css('display', 'block');
+  //     } else {
+  //       $('.service-info__text').css('display', 'none');
+  //     }
+  //   });
 
   $(window).on('scroll', function () {
     if ($(window).scrollTop() >= 200) {
@@ -56,6 +84,11 @@ $(document).ready(function () {
     }
   });
 
-
+  function scrollOnNewPage(id) {
+    document.location.href = 'index.html#' + id;
+    // $('html, body').animate({
+    //   scrollTop: $(id).offset().top - 100
+    // }, 500);
+  }
 
 });
